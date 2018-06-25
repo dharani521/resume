e9757f34ae8043e5a8419a517908b83a9ee6e98a
dharu <dharanigunupudi@gmail.com>
2018-06-25 04:35:41 -0700

    first commit12

--- main.js
+++ main.js
@@ -12,14 +12,14 @@ function loadJson(file,callback){
 loadJson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
-//  fun_career(data.career);
+  fun_career(data.career);
 fun_education(data.education);
-//  fun_achievements(data.achievements);
-//  fun_skills(data.skills);
+ //  fun_achievements(data.achievements);
+ // fun_skills(data.skills);
   });
   var div2=document.getElementById("child2");
 
-/*  function fun_career(car){
+  function fun_career(car){
     var heading1=document.createElement("h2");
     heading1.textContent="CAREER OBJECTIVE";
     div2.appendChild(heading1);
@@ -29,7 +29,7 @@ fun_education(data.education);
     info.textContent=car.info;
     div2.appendChild(info);
 
-  }*/
+  }
  function fun_education(edu){
     var list1=document.createElement("h2");
     list1.textContent="EDUCATIONAL QUALIFICATIONS";
@@ -50,3 +50,32 @@ fun_education(data.education);
       list.appendChild(listItem2);
     }
   }
+  function fun_achievements(achieve){
+  var heading2=document.createElement("h2");
+  heading2.textContent="ACHIEVEMENTS";
+  div2.appendChild(heading2);
+  var horz=document.createElement("hr");
+  heading2.appendChild(horz);
+  var ul=document.createElement("ul");
+  div2.appendChild(ul);
+    for(var i=0;i<achieve.length;i++){
+    var listItem3=document.createElement("li");
+    listItem3.innerHTML=achieve[i].name;
+    ul.appendChild(listItem3);
+  }
+}
+function fun_skills(tech){
+  var heading=document.createElement("h2");
+  heading.textContent=" TECHNICAL SKILLS";
+  div2.appendChild(heading);
+  var hr=document.createElement("hr");
+  heading.appendChild(hr);
+  var tr="";
+  var table=document.createElement("table");
+  div2.appendChild(table);
+    for(var i=0;i<tech.length;i++){
+      tr+="<tr><td>"+tech[i].name+"</td><td>"+tech[i].data+"</td></tr>";
+    }
+    table.innerHTML=tr;
+    table.border="1";
+}
